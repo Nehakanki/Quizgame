eaca7486a80a8b11f089d0bc3c5697d58511c87e
Neha <kankineha@gmail.com>
2023-06-05 19:46:49 +0530

    Quiz game

---
+++ quiz.java
@@ -0,0 +1,9 @@
+public class quiz {
+    @FXML
+    private Button playquizbtn;
+
+    @FXML
+    private void initialize(){
+        playquizbtn.setOnAction(new EventHandler<ActionEvent>())
+    }
+}
